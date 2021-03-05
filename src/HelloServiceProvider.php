2366be55bfbf09f8849143051f20730afa0cead3
viendd <viendd@viralsoft.vn>
2021-03-05 14:41:56 +0700

    add package

---
+++ src/HelloServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Viralsoft\Hello;
+
+use Illuminate\Support\ServiceProvider;
+
+class HelloServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadRoutesFrom(realpath(__DIR__ . '/routes/web.php'));
+    }
+}
